f88949eafb640a3d3409868ac98500c97f435a0a
rendhive <rendhivecode@gmail.com>
2025-12-06 14:55:09 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul configparser.py

---
+++ 1. Mengimpor Modul configparser.py
@@ -0,0 +1,5 @@
+import configparser
+
+print("Modul configparser berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk membaca dan menulis file konfigurasi.
+# Kondisi: Digunakan sebelum memanfaatkan fungsionalitas dari configparser.
